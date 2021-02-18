06cea49332889b0040cf1fcc85ee7a023858fe67
MiSalocin <61383781+MiSalocin@users.noreply.github.com>
2021-02-18 18:28:19 -0300

    Created a class to move the robot using the IMU Creating a way to make the motor don't make abrupt movements.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MMCore.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MMCore.java
@@ -4,38 +4,31 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "MMCore", group = "Bahtech")
 public class MMCore extends LinearOpMode {
 
     // Map the hardware to autonomus and teleOp
     final MMMovement move = new MMMovement();
-    double x = 0.8, y = 0;
 
     @Override
     public void runOpMode() {
 
         // Create the Hardware Map and start IMU
         move.defHardware(hardwareMap);
-        move.startIMU();
-
-        //DcMotor shooter = hardwareMap.dcMotor.get("shooter_motor");
-        //Servo shooT = hardwareMap.servo.get("shooter_trig_servo");
-
+        move.turn(90, 1, 0.9);
+        move.turn(90,0, 0.6);
         // Run once you press PLAY
         waitForStart();
 
-        telemetry.addData("Aperte START para definir a frente a partir do Robô e BACK para a da arena", "");
-        while (y==0) {
-            if (gamepad1.back)  y = 1;
-            if (gamepad1.start) y = 2;}
+        // Program used to define if it will base itself using the robot or the arena
+        double y = 0;
 
         // Run repeatly after you press play
         while(opModeIsActive()){
-            telemetry.update();
 
+            if (gamepad1.back)  y = 1;
+            if (gamepad1.start) y = 2;
 
             // Create variables from the control
             double leftX = gamepad1.left_stick_x;
@@ -44,19 +37,16 @@ public class MMCore extends LinearOpMode {
             boolean lb = gamepad1.left_bumper;
             boolean rb = gamepad1.right_bumper;
 
-            telemetry.addData ("LextX: ", leftX);
-            telemetry.addData ("LextY: ", leftY);
-            telemetry.addData ("RightX: ", rightX);
+            if (y != 0) telemetry.addData ("LextX: ", leftX);
+            if (y != 0) telemetry.addData ("LextY: ", leftY);
+            if (y != 0) telemetry.addData ("RightX: ", rightX);
 
-            if (y == 0) telemetry.addData("", move.moveByArena(leftX, leftY, rightX, lb, rb));
+            if (y == 1) move.moveByArena(leftX, leftY, rightX, lb, rb);
             if (y == 2) move.MoveByRobot(leftX, leftY, rightX, lb, rb);
+            if (y == 0) telemetry.addData("Aperte START para definir a frente a partir do ",
+                    "Robô e BACK para a da arena");
 
-            //if (gamepad1.x) x = 0.75;
-            //if (gamepad1.y) x = 0.8;
-            //shooter.setPower(x);
-            //if (gamepad1.a) shooT.setPosition(1);
-            //else   shooT.setPosition(0);
-
+            telemetry.update();
         }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MMMovement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MMMovement.java
@@ -4,21 +4,13 @@ import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-// import com.qualcomm.robotcore.hardware.Servo;
 
-import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
-import java.util.Vector;
-
 public class MMMovement {
-
-    // Variables used in the shooter and the claw
-    // private int    x = 0;
-    // private double y = 0.8;
 
     // Motors that will be used in the movement
     private DcMotor frontRight,
@@ -26,25 +18,36 @@ public class MMMovement {
             backRight,
             backLeft;
 
-    // Defining motor for claw and shooter
-    // Motors that will be used in the claw
-    // private DcMotor arm;
-    // private Servo hand;
-
-    // Starts the IMU variables
+    // Starts the IMU
     private BNO055IMU imu;
     private final BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
     private double inter = 1;
     private double exter = 1;
 
-    final private Vector<Double> force = new Vector<>();
-    final private Vector<Double> lastForce = new Vector<>();
+
+    // Create vectors to define the forces with less variables
+    double flforce = 0;
+    double frforce = 0;
+    double blforce = 0;
+    double brforce = 0;
+
+    double lastflforce = 0;
+    double lastfrforce = 0;
+    double lastblforce = 0;
+    double lastbrforce = 0;
 
     // Program used to define the hardware variables
     void defHardware (HardwareMap local) {
 
         // Define IMU
         imu = local.get(BNO055IMU.class, "imu");
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        imu.initialize(parameters);
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
 
         // Motors used in the movement
         frontLeft = local.dcMotor.get("front_left_motor");
@@ -55,113 +58,129 @@ public class MMMovement {
         // Reverse right motors to make programming easier
         frontRight.setDirection(DcMotor.Direction.REVERSE);
         backRight.setDirection(DcMotor.Direction.REVERSE);
-
-        /*Specify the claw and shooter motors
-         // Motors that wil be used in the claw
-         arm = local.dcMotor.get("arm_motor");
-         hand = local.servo.get("hand_servo");
-         arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-         arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-         */
-    }
-
-    // Define the IMU parameter that will be used when it is used
-    void startIMU (){
-        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
-        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
-        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
-        parameters.loggingEnabled = true;
-        parameters.loggingTag = "IMU";
-        imu.initialize(parameters);
-        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
     }
 
     // Program used to move the robot using the arena
-    double moveByArena(double leftY, double leftX, double rightX, boolean slower, boolean faster){
+    void moveByArena(double leftY, double leftX, double rightX, boolean slower, boolean faster){
 
+        // Create a variable using the IMU
         Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
         double angle = angles.firstAngle;
 
-        if (0 < angle && angle <= 90)      {inter = -(angle/45-1); exter = 1;}
-        else if (90 < angle && angle <= 180)    {inter = -1; exter = -((angle-90)/45-1);}
-        else if (-90 < angle && angle <= 0)     {inter = 1; exter = angle/45+1;}
-        else if (-180 < angle && angle <= 90)   {inter = (angle+90)/45+1; exter = -1;}
+        // Will see the IMU value and change the "inter" and "exter" variaibles
+        if      (   0 < angle && angle <= 90 )   {inter = -(angle/45-1);    exter = 1;}
+        else if (  90 < angle && angle <= 180)   {inter = -1;               exter = -((angle-90)/45-1);}
+        else if ( -90 < angle && angle <= 0  )   {inter = 1;                exter = angle/45+1;}
+        else if (-180 < angle && angle <= 90 )   {inter = (angle+90)/45+1;  exter = -1;}
 
+        // Will start the normal "MoveByRobot" program, but using the Inter and Exter Multipliers
         MoveByRobot(leftY, leftX, rightX, slower, faster);
-        return angle;
     }
 
     // Program used to move the robot by himself
     void MoveByRobot (double leftY, double leftX, double rightX, boolean slower, boolean faster){
 
-        force.add(0, leftY * inter - leftX * exter - rightX);
-        force.add(1, leftY * exter + leftX * inter - rightX);
-        force.add(2, leftY * exter + leftX * inter + rightX);
-        force.add(3, leftY * inter - leftX * exter + rightX);
 
-        if (Math.abs(lastForce.get(0)-force.get(0)) > 0.25){
-            if (lastForce.get(0) > force.get(0))    force.add(0, lastForce.get(0)-0.25);
-            else                                    force.add(0, lastForce.get(0)+0.25);}
-        if (Math.abs(lastForce.get(1)-force.get(1)) > 0.25){
-            if (lastForce.get(1) > force.get(1))    force.add(1, lastForce.get(1)-0.25);
-            else                                    force.add(1, lastForce.get(1)+0.25);}
-        if (Math.abs(lastForce.get(2)-force.get(2)) > 0.25){
-            if (lastForce.get(2) > force.get(2))    force.add(2, lastForce.get(2)-0.25);
-            else                                    force.add(2, lastForce.get(2)+0.25);}
-        if (Math.abs(lastForce.get(0)-force.get(0)) > 0.25){
-            if (lastForce.get(3) > force.get(3))    force.add(3, lastForce.get(3)-0.25);
-            else                                    force.add(3, lastForce.get(3)+0.25);}
+        // Add to the vector the forces of the gamepad multiplying this with the angle defined
+        // in the "MoveByArena" program.
+        flforce = leftY * inter - leftX * exter - rightX;
+        frforce = leftY * exter + leftX * inter - rightX;
+        blforce = leftY * exter + leftX * inter + rightX;
+        brforce = leftY * inter - leftX * exter + rightX;
 
-        lastForce.add(0, force.get(0));
-        lastForce.add(1, force.get(1));
-        lastForce.add(2, force.get(2));
-        lastForce.add(3, force.get(3));
+        // See if the diference of the last force and the current one is bigger than 0.1,
+        // if it is, it will change gradually to not damage the motors
+        if (Math.abs(lastflforce - flforce) > 0.15 || Math.abs(lastflforce - flforce) < 0.15){
+            if (lastflforce > flforce)              flforce = lastflforce - 0.15;
+            else                                    flforce = lastflforce + 0.15;}
+
+        if (Math.abs(lastfrforce - frforce) > 0.15 || Math.abs(lastfrforce - frforce) < 0.15){
+            if (lastfrforce > frforce)              frforce = lastfrforce - 0.15;
+            else                                    frforce = lastfrforce + 0.15;}
+
+        if (Math.abs(lastblforce - blforce) > 0.15 || Math.abs(lastblforce - blforce) < 0.15){
+            if (lastblforce > blforce)              blforce = lastblforce - 0.15;
+            else                                    blforce = lastblforce + 0.15;}
+
+        if (Math.abs(lastbrforce - brforce) > 0.15 || Math.abs(lastbrforce - brforce) < 0.15){
+            if (lastbrforce > brforce)              brforce = lastbrforce - 0.15;
+            else                                    brforce = lastbrforce + 0.15;}
+
+        // Save the used force in variables to get the diference
+        lastflforce = flforce;
+        lastfrforce = frforce;
+        lastblforce = blforce;
+        lastbrforce = brforce;
 
         // If the right bumper is pressed, the robot will move slower
         if (slower) {
-            frontRight.setPower(force.get(0) / 4);
-            backRight .setPower(force.get(1) / 4);
-            frontLeft .setPower(force.get(2) / 4);
-            backLeft  .setPower(force.get(3) / 4);
+            frontRight.setPower(flforce / 4);
+            backRight .setPower(frforce / 4);
+            frontLeft .setPower(blforce / 4);
+            backLeft  .setPower(brforce / 4);
         }
         // If the right bumper is pressed, the robot will move faster
         else if (faster) {
-            frontRight.setPower(force.get(0));
-            backRight .setPower(force.get(1));
-            frontLeft .setPower(force.get(2));
-            backLeft  .setPower(force.get(3));
+            frontRight.setPower(flforce);
+            backRight .setPower(frforce);
+            frontLeft .setPower(blforce);
+            backLeft  .setPower(brforce);
         }
         // If neither bumper is pressed, the robot will move half the speed
         else {
-            frontRight.setPower(force.get(0) / 2);
-            backRight .setPower(force.get(1) / 2);
-            frontLeft .setPower(force.get(2) / 2);
-            backLeft  .setPower(force.get(3) / 2);
+            frontRight.setPower(flforce / 2);
+            backRight .setPower(frforce / 2);
+            frontLeft .setPower(blforce / 2);
+            backLeft  .setPower(brforce / 2);
         }
     }
-    //  Program used in the shot and claw
-    //  void moveClaw(boolean up, boolean down, boolean openHand, boolean closeHand){
-    //
-    //  // move up or down the claw
-    //  if (up   && -1   >= arm.getCurrentPosition()) {
-    //  arm.setTargetPosition(x+=5);
-    //  arm.setPower(1);
-    //  }else if (down && -653 <= arm.getCurrentPosition()) {
-    //  arm.setTargetPosition(x-=5);
-    //  arm.setPower(1);
-    //  }else arm.setPower(0);
-    //
-    //  // Open or close the robot's "hand"
-    //  if (openHand  && y < 1) hand.setPosition(y += 0.05);
-    //  if (closeHand && y > 0) hand.setPosition(y -= 0.05);
-    //
-    //  }
-    //
-    //  void shot(boolean trigger, double force){
-    //
-    //  shooter.setPower(force);
-    //  if (trigger) shooT.setPosition(1);
-    //  else   shooT.setPosition(0);
-    //
-    // }
+
+    // Program used to turn the robot to one side. With this, you can
+    // define the angle in degrees, if it shout turn left or right and the force
+    void turn(double angle, double right, double force) {
+        imu.initialize(parameters);
+        double currentAngle = 0;
+        if (right==1){
+            while (-angle <= currentAngle) {
+                Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+                currentAngle = angles.firstAngle;
+
+                if (force < (angle-currentAngle/100)) {
+                    frontRight.setPower(-force);
+                    backRight. setPower(-force);
+                    frontLeft. setPower( force);
+                    backLeft.  setPower( force);
+                }else{
+                    frontRight.setPower(-(angle-currentAngle)/100);
+                    backRight. setPower(-(angle-currentAngle)/100);
+                    frontLeft. setPower( (angle-currentAngle)/100);
+                    backLeft.  setPower( (angle-currentAngle)/100);
+                }
+
+
+            }
+        }
+        else{
+            while (angle >= currentAngle) {
+                Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+                currentAngle = angles.firstAngle;
+
+                if (force < (angle-currentAngle/100)) {
+                    frontRight.setPower( force);
+                    backRight. setPower( force);
+                    frontLeft. setPower(-force);
+                    backLeft.  setPower(-force);
+                }else{
+                    frontRight.setPower( (angle-currentAngle)/100);
+                    backRight. setPower( (angle-currentAngle)/100);
+                    frontLeft. setPower(-(angle-currentAngle)/100);
+                    backLeft.  setPower(-(angle-currentAngle)/100);
+                }
+            }
+            frontRight.setPower(0);
+            backRight .setPower(0);
+            frontLeft .setPower(0);
+            backLeft  .setPower(0);
+        }
+    }
 }
